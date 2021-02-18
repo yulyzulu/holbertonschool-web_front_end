830ec23f4df30034b1fa468d29ebea5194ad9e2d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-02-17 21:00:14 -0500

    Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,22 @@
+function countPrimeNumbers() {
+    let count = 0;
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+        for (let j = 2; j <= i; j++) {
+            if (i % j === 0 && j !== i) {
+                isPrime = false;
+            }
+        }
+        if (isPrime === true) {
+            count = count + 1;
+        }
+    }
+    return count;
+}
+
+var t0 = performance.now();
+for (let x = 0; x < 100; x++) {
+    countPrimeNumbers();
+}
+var t1 = performance.now();
+console.log("Execution time of calculating prime numbers 100 times was " + (t1 - t0) + " milliseconds.")
